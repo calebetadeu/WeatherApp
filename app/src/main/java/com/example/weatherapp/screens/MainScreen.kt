597318faa6b2bcb3959232bb86d683724d694b4c
Calebe Tadeu Matos da Silva <calebe.silva@efrete.com.br>
2022-09-15 12:04:55 -0300

    Weather navigatio on mainActivity

---
+++ app/src/main/java/com/example/weatherapp/screens/MainScreen.kt
@@ -0,0 +1,9 @@
+package com.example.weatherapp.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen() {
+    Text("MainScreen")
+}
